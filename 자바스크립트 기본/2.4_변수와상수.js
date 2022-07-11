86170e8e520a035ea8d95095ac2d5924fbe498a4
Miintoo <xpeed96@gmail.com>
2022-07-11 18:30:49 +0900

    2.4 변수와 상수 예제 제출

---
+++ 자바스크립트 기본/2.4_변수와상수.js
@@ -0,0 +1,24 @@
+
+// 1-1.
+let admin;
+let name;
+
+// 1-2.
+name = 'John';
+
+// 1-3.
+admin = name;
+
+// 1-4.
+alert(admin);
+
+// 2-1.
+let ourPlanetName = 'Earth';
+
+// 2-2.
+let currentUserName;
+
+// 3
+
+// Birthday 변수는 변하지 않는 상수 값이기 때문에 대문자로 변경해도 된다.
+// age는 시간이 지나갈수록 바뀌는 값이기 때문에 상수가 아닌 변수로 선언하는게 좋을꺼같다.
